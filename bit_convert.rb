49383a30d6e870e88fb7a8b6290fdfff306b816a
ViolaCello <ViolaCelloNYC@gmail.com>
2021-12-27 14:33:14 -0500

    decimal_to_binary_test works up to 9999999

--- bit_convert.rb
+++ bit_convert.rb
@@ -10,4 +10,22 @@ def random_array(n, up_to)
     return result
 end
 
-p random_array(25, 100)
+def decimal_to_binary_test(n)
+    old_n = n 
+    result = ""
+    while n>0 do
+        result = (n%2).to_s + result
+        n /= 2
+    end
+    return [result==old_n.to_s(2)]
+end
+
+# This confirms that it works up to number 9999999
+arr = random_array(10000, 9999999)
+arr.each do |x| 
+puts decimal_to_binary_test(x)
+if x==false
+    puts "BAD!!!"
+    break
+end
+end
